cb301a80cc6f42b79ac5c463be726ae5ddb4604a
KonovalovDM <kdm232006@gmail.com>
2024-10-28 23:34:37 +0300

    add new code in branch sudar

--- kurtki_zimnie/kurtki_zimnie/spiders/kurtki_zimnie_pars.py
+++ kurtki_zimnie/kurtki_zimnie/spiders/kurtki_zimnie_pars.py
@@ -1,16 +1,30 @@
 import scrapy
-
+import csv
 
 class KurtkiZimnieparsSpider(scrapy.Spider):
-    name = "kurtki_zimnie_pars" #Указываем название
-    allowed_domains = ["sudar.su"]  # Указываем только домен
+    name = "kurtki_zimnie_pars"
+    allowed_domains = ["sudar.su"]
     start_urls = ["https://sudar.su/catalog/kurtki_zimnie/"]
+
+    custom_settings = {
+        'FEED_EXPORT_ENCODING': 'utf-8-sig'  # Устанавливаем кодировку для записи CSV
+    }
 
     def parse(self, response):
         kurtki = response.css("div.col-md-12")
+
         for kurtka in kurtki:
-            yield {
-                'name' : kurtka.css('div.b-card__title p::text').get(),
-                'price' : kurtka.css('div.b-price b::text').get(),
-                'url' : kurtka.css('a').attrib['href'] #Используем response.urljoin
-            }
+            try:
+                name = kurtka.css('div.b-card__title p::text').get()
+                price = kurtka.css('div.b-price b::text').get()
+                url = response.urljoin(kurtka.css('a').attrib['href'])
+
+                yield {
+                    'name': name,
+                    'price': price,
+                    'url': url
+                }
+
+            except Exception as e:
+                self.logger.error("Произошла ошибка при парсинге данных: %s", e)
+                continue
