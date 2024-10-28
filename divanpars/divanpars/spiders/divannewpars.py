3f6b5a2cadb80d4cf98b0f963ccb9d2c0c4baaef
KonovalovDM <kdm232006@gmail.com>
2024-10-29 00:25:53 +0300

    add new code

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -3,20 +3,20 @@ import csv
 
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
-    allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+    allowed_domains = ["divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     custom_settings = {
         'FEED_EXPORT_ENCODING': 'utf-8-sig'  # Устанавливаем кодировку для записи CSV
     }
 
     def parse(self, response):
-        divans = response.css("div._Ud0k")
-        for divan in divans:
+        svets = response.css("div.WdR1o")
+        for svet in svets:
             try:
-                name = divan.css('div.lsooF span::text').get(),
-                price = divan.css('div.pY3d2 span::text').get(),
-                url = response.urljoin(divan.css('a').attrib['href'])
+                name = svet.css('div.lsooF span::text').get(),
+                price = svet.css('div.pY3d2 span::text').get(),
+                url = response.urljoin(svet.css('a').attrib['href'])
 
                 yield {
                 'name' : name,
