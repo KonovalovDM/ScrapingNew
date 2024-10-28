e31f55f4c2b2b225ad022f7bec652759c1f3c543
KonovalovDM <kdm232006@gmail.com>
2024-10-29 00:11:06 +0300

    add new code

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -1,17 +1,30 @@
 import scrapy
-
+import csv
 
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/category/svet"]
+    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+
+    custom_settings = {
+        'FEED_EXPORT_ENCODING': 'utf-8-sig'  # Устанавливаем кодировку для записи CSV
+    }
 
     def parse(self, response):
-        divans = response.css("div.WdR1o")
+        divans = response.css("div._Ud0k")
         for divan in divans:
-            yield {
-                'name' : divan.css('div.lsooF span::text').get(),
-                'price' : divan.css('div.pY3d2 span::text').get(),
-                'url' : divan.css('a').attrib['href']
-            }
+            try:
+                name = divan.css('div.lsooF span::text').get(),
+                price = divan.css('div.pY3d2 span::text').get(),
+                url = response.urljoin(divan.css('a').attrib['href'])
+
+                yield {
+                'name' : name,
+                'price' : price,
+                'url' : url
+                }
+
+            except Exception as e:
+                self.logger.error("Произошла ошибка при парсинге данных: %s", e)
+                continue
 
